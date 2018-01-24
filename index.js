935ece2919d281a2cf84e23e6d7612dbde30a936
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-01-24 13:38:56 -0800

    contrast values saved to local storage

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-function modifyString(value) {
-  return 'better ' + value;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('hello world');
-  var inputBox = document.getElementById('contrast-input');
+  // Inputs
+  var username = document.getElementById('contrast_username');
+  var serviceKey = document.getElementById('contrast_service_key');
+  var apiKey = document.getElementById('contrast_api_key');
+  var orgUuid = document.getElementById('contrast_org');
+  var teamserverUrl = document.getElementById('teamserver_url');
+
   var submitButton = document.getElementById('contrast-submit');
 
+  // Run when form is submitted
   submitButton.addEventListener('click', function() {
-    value = inputBox.value;
-    newValue = modifyString(value);
-    console.log(newValue);
+    // retrieve values form inputs
+    usernameValue = username.value;
+    serviceKeyValue = serviceKey.value;
+    apiKeyValue = apiKey.value;
+    orgUuidValue = orgUuid.value;
+    teamserverUrlValue = teamserverUrl.value;
+
+    //save values to local storage
+    chrome.storage.sync.set({
+      'contrast_username': usernameValue,
+      'contrast_service_key': serviceKeyValue,
+      'contrast_api_key': apiKeyValue,
+      'contrast_org': orgUuidValue,
+      'teamserver_url': teamserverUrlValue
+    }, function() {
+      console.log('Contrast values saved');
+      username.value = "";
+      serviceKey.value = "";
+      apiKey.value = "";
+      orgUuid.value = "";
+      teamserverUrl.value = "";
+
+    });
   }, false);
 
 }, false);
