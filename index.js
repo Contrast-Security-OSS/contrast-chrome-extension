ff98f28880fec82bbd25780d1425a95860ca62b6
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-01-24 14:38:58 -0800

    signin button creates new tab

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Inputs
-  var username = document.getElementById('contrast_username');
-  var serviceKey = document.getElementById('contrast_service_key');
-  var apiKey = document.getElementById('contrast_api_key');
-  var orgUuid = document.getElementById('contrast_org');
-  var teamserverUrl = document.getElementById('teamserver_url');
-
-  var submitButton = document.getElementById('contrast-submit');
-
-  // Run when form is submitted
-  submitButton.addEventListener('click', function() {
-    // retrieve values form inputs
-    usernameValue = username.value;
-    serviceKeyValue = serviceKey.value;
-    apiKeyValue = apiKey.value;
-    orgUuidValue = orgUuid.value;
-    teamserverUrlValue = teamserverUrl.value;
-
-    //save values to local storage
-    chrome.storage.sync.set({
-      'contrast_username': usernameValue,
-      'contrast_service_key': serviceKeyValue,
-      'contrast_api_key': apiKeyValue,
-      'contrast_org': orgUuidValue,
-      'teamserver_url': teamserverUrlValue
-    }, function() {
-      console.log('Contrast values saved');
-      username.value = "";
-      serviceKey.value = "";
-      apiKey.value = "";
-      orgUuid.value = "";
-      teamserverUrl.value = "";
-
-    });
+  var signInButton = document.getElementById('sign-in');
+  var extensionId = chrome.runtime.id;
+  signInButton.addEventListener('click', function() {
+    var settingsUrl = 'chrome-extension://' + String(extensionId) + '/settings.html';
+    chrome.tabs.create({url: settingsUrl});
   }, false);
-
 }, false);
