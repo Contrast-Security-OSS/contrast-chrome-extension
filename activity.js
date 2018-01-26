b61c268e2e4f747704d49854e094c100592e84b4
Gary Moore <gary.moore@contrastsecurity.com>
2018-01-25 17:22:16 -0800

    Now with style

--- activity.js
+++ activity.js
@@ -1,41 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
-  ContrastFetch("/ng/e264d365-25e4-409e-a129-ec4c684c9d50/events", function() { return function(e) {
-    xhr = e.currentTarget
-    if (xhr.readyState == 4) {
-      json = JSON.parse(xhr.responseText)
-      //console.log(json["activities"])
+  chrome.storage.sync.get(["contrast_service_key"], function(items) {
 
-      activities = json["activities"]
-      $.each(activities, function(idx, activity){
-        console.log(activity)
-        var text = activity["description"]["text"]
-        
+    if (items["contrast_service_key"] != null) {
 
-        var desc = ""
+      ContrastFetch("/ng/e264d365-25e4-409e-a129-ec4c684c9d50/events", function() { return function(e) {
+        xhr = e.currentTarget
+        if (xhr.readyState == 4) {
+          if(xhr.responseText == "" ) {
+            $("body").addClass("no-activity")
+            $("#get-started").show()
+          } else {
 
-        if(activity["type"] == "NEW_TRACE") {
-          desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14, text.lastIndexOf("$$LINK_DELIM$$"))
-        } else {
-          desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14)
-        } 
-        
+            json = JSON.parse(xhr.responseText)
+            //console.log(json["activities"])
 
-        var li = $('<li/>')
-          .addClass('list-group-item')
-          .appendTo($("#contrast-events"));
+            activities = json["activities"]
+            $.each(activities, function(idx, activity){
+              console.log(activity)
+              var text = activity["description"]["text"]
+              
 
-        li.append($('<h6/>').append(activity["type"].replace(/_/g, " ")))
-        li.append($('<div/>').append(desc))
+              var desc = ""
 
-        var date = new Date(null);
-        date.setMilliseconds(activity["timestamp"]) // specify value for SECONDS here
-        dateVal = date.toString()
+              if(activity["type"] == "NEW_TRACE") {
+                desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14, text.lastIndexOf("$$LINK_DELIM$$"))
+              } else {
+                desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14)
+              } 
+              
+
+              var li = $('<li/>')
+                .addClass('list-group-item')
+                .appendTo($("#contrast-events"));
+
+              li.append($('<h6/>').append(activity["type"].replace(/_/g, " ")))
+              li.append($('<div/>').append(desc))
+
+              var date = new Date(null);
+              date.setMilliseconds(activity["timestamp"]) // specify value for SECONDS here
+              dateVal = date.toString()
 
 
-        li.append($('<h6/>').append(dateVal))
+              li.append($('<h6/>').append(dateVal))
+            })
 
-      })
+          }
+        }
+      }})
     }
-  }})
+  })
 
 }, false);
