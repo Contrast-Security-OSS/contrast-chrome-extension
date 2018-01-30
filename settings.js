619b53a6ca0c8c1bcb45ec698c727d4bfc55ada2
Segii Borsuk <sergii.borsuk@contrastsecurity.com>
2018-01-29 19:08:31 -0600

    CONTRAST-20370 When viewing the setting screen, if a user has already configured the plugin, the settings should appear on the setting

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Inputs
   var username = document.getElementById('contrast_username');
   var serviceKey = document.getElementById('contrast_service_key');
@@ -6,10 +6,24 @@ document.addEventListener('DOMContentLoaded', function() {
   var orgUuid = document.getElementById('contrast_org_uuid');
   var teamserverUrl = document.getElementById('teamserver_url');
 
+
+  chrome.storage.sync.get(["contrast_username",
+    "contrast_service_key",
+    "contrast_api_key",
+    "contrast_org_uuid",
+    "teamserver_url"], function (items) {
+      username.setAttribute("value", items["contrast_username"]);
+      serviceKey.setAttribute("value", items["contrast_service_key"]);
+      apiKey.setAttribute("value", items["contrast_api_key"]);
+      orgUuid.setAttribute("value", items["contrast_org_uuid"]);
+      teamserverUrl.setAttribute("value", items["teamserver_url"]);
+    });
+
+
   var submitButton = document.getElementById('contrast-submit');
 
   // Run when form is submitted
-  submitButton.addEventListener('click', function() {
+  submitButton.addEventListener('click', function () {
     // retrieve values form inputs
     usernameValue = username.value;
     serviceKeyValue = serviceKey.value;
@@ -24,10 +38,10 @@ document.addEventListener('DOMContentLoaded', function() {
       'contrast_api_key': apiKeyValue,
       'contrast_org_uuid': orgUuidValue,
       'teamserver_url': teamserverUrlValue
-    }, function() {
+    }, function () {
       console.log('Contrast values saved');
-      chrome.tabs.getCurrent(function(tab) {
-        chrome.tabs.remove(tab.id, function() { });
+      chrome.tabs.getCurrent(function (tab) {
+        chrome.tabs.remove(tab.id, function () { });
       });
 
 
