8497582ef634f45bc3ad211cfe1765853bf07fbd
Segii Borsuk <sergii.borsuk@contrastsecurity.com>
2018-01-31 12:19:19 -0600

    CONTRAST-20369 In the Activity Feed, click on the timestamp to go to the event in teamserver

--- activity.js
+++ activity.js
@@ -1,52 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.sync.get(["contrast_service_key"], function(items) {
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.storage.sync.get(["contrast_service_key"], function (items) {
 
     if (items["contrast_service_key"] != null) {
 
-      getActivities(function() { return function(e) {
-        xhr = e.currentTarget
-        if (xhr.readyState == 4) {
-          if(xhr.responseText == "" ) {
-            $("body").addClass("no-activity")
-            $("#get-started").show()
-          } else {
+      getActivities(function () {
+        return function (e) {
+          xhr = e.currentTarget
+          if (xhr.readyState == 4) {
+            if (xhr.responseText == "") {
+              $("body").addClass("no-activity")
+              $("#get-started").show()
+            } else {
 
-            json = JSON.parse(xhr.responseText)
-            //console.log(json["activities"])
+              json = JSON.parse(xhr.responseText)
+              //console.log(json["activities"])
+              console.log(xhr.responseText);
 
-            activities = json["activities"]
-            $.each(activities, function(idx, activity){
-              console.log(activity)
-              var text = activity["description"]["text"]
-              
-
-              var desc = ""
-
-              if(activity["type"] == "NEW_TRACE") {
-                desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14, text.lastIndexOf("$$LINK_DELIM$$"))
-              } else {
-                desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14)
-              } 
-              
-
-              var li = $('<li/>')
-                .addClass('list-group-item')
-                .appendTo($("#contrast-events"));
-
-              li.append($('<h6/>').append(activity["type"].replace(/_/g, " ")))
-              li.append($('<div/>').append(desc))
-
-              var date = new Date(null);
-              date.setMilliseconds(activity["timestamp"]) // specify value for SECONDS here
-              dateVal = date.toString()
+              activities = json["activities"]
+              $.each(activities, function (idx, activity) {
+                console.log(activity)
+                var text = activity["description"]["text"]
 
 
-              li.append($('<h6/>').append(dateVal))
-            })
+                var desc = ""
 
+                if (activity["type"] == "NEW_TRACE") {
+                  desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14, text.lastIndexOf("$$LINK_DELIM$$"))
+                } else {
+                  desc = text.substring(text.indexOf("$$LINK_DELIM$$") + 14)
+                }
+
+
+                var li = $('<li/>')
+                  .addClass('list-group-item')
+                  .appendTo($("#contrast-events"));
+
+                li.append($('<h6/>').append(activity["type"].replace(/_/g, " ")))
+                li.append($('<div/>').append(desc))
+
+                var date = new Date(null);
+                date.setMilliseconds(activity["timestamp"]) // specify value for SECONDS here
+                var dateVal = date.toString();
+
+                li.append($('<h6/>').append(dateVal));
+              })
+
+              $('#contrast-events').click(function (event) {
+                var text = $(event.target).text();
+                console.log(text);
+              });
+
+            }
           }
         }
-      }})
+      })
     }
   })
 
