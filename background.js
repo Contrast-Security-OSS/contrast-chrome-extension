8d6b63c6841d04ba40167ac1260e611f0ae307b2
Your Name <you@example.com>
2018-02-07 10:30:22 -0800

    CONTRAST-20418

--- background.js
+++ background.js
@@ -5,11 +5,25 @@ chrome.tabs.onActivated.addListener(onActivatedListener);
 
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
-		if (request.traces.length > 0) {
-			chrome.browserAction.setBadgeText({ text: request.traces.length.toString() });
-		} else {
-			chrome.browserAction.setBadgeText({ text: "" });
+		if (request.traces && request.traces.length > 0) {
+
+			chrome.tabs.get(sender.tab.id, function (tab) {
+				if (chrome.runtime.lastError) {
+					return;
+				}
+				if (tab.index >= 0) { // tab is visible
+					chrome.browserAction.setBadgeText({ tabId: tab.id, text: request.traces.length.toString() });
+				} else {
+					var tabId = sender.tab.id, text = request.traces.length.toString();
+					chrome.webNavigation.onCommitted.addListener(function update(details) {
+						if (details.tabId == tabId) {
+							chrome.browserAction.setBadgeText({ tabId: tabId, text: text });
+							chrome.webNavigation.onCommitted.removeListener(update);
+						}
+					});
+				}
+			});
+			// sendResponse({message: "message"});
 		}
-		// sendResponse({message: "message"});
 	}
 );
