6d8667a277990803c42191064c5486f4695d858b
Dave Corderman <david.corderman@contrastsecurity.com>
2018-04-03 15:57:00 -0400

    cleaned up background and added jsdocs to methods

--- js/background.js
+++ js/background.js
@@ -16,96 +16,105 @@ CONTRAST_USERNAME,
 
 // called before any sync or async request is sent
 // captures xhr and resource requests
+
+/**
+ * chrome - description
+ *
+ * @param  {Function} function - callback
+ * @param {Object} filter - allows limiting the requests for which events are triggered in various dimensions including urls
+ * @return {void}
+ * Called before any web request takes place XHR or otherwise
+ */
 chrome.webRequest.onBeforeRequest.addListener(function(request) {
+
 	// only permit xhr requests
 	// don't monitor xhr requests made by extension
 	if (request.type === "xmlhttprequest" && !request.url.includes("Contrast")) {
 	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 		var tab = tabs[0]
-		// console.log("request object", request);
-		// if (!!tab && tab.status === "complete" && tab.url.startsWith("http")) {
+
 			chrome.storage.sync.get([CONTRAST_USERNAME, CONTRAST_SERVICE_KEY, CONTRAST_API_KEY, TEAMSERVER_URL], function (items) {
 
 				if (tab.url.includes("Contrast/api")) {
 					return;
 				}
-				evaluateVulnerabilities(checkCredentials(items), tab, request.url)
+
+				evaluateVulnerabilities(isCredentialed(items), tab, request.url)
 			})
 		})
 	}
 }, { urls: [LISTENING_ON_DOMAIN] })
 
-// called when tab is updated including any changes to url
-// build array of vulnerabilities
+/**
+ * anonymous function - called when tab is updated including any changes to url
+ *
+ * @param  {Integer} tabId     the chrome defined id of the tab
+ * @param  {Object} changeInfo Lists the changes to the state of the tab that was updated.
+ * @param  {Object} tab        Gives the state of the tab that was updated.
+ * @return {void}
+ */
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	console.log("tab updated", tab);
+
 	// send message to content scripts that tab has updated
-	// get forms
 	removeVulnerabilitiesFromStorage().then(function() {
 		if (changeInfo.status === "complete" && tab.url.startsWith("http")) {
 
 			chrome.storage.sync.get([CONTRAST_USERNAME, CONTRAST_SERVICE_KEY, CONTRAST_API_KEY, TEAMSERVER_URL], function (items) {
+				var credentialed = isCredentialed(items)
+				if (credentialed) {
 
-				// gets form actions
-				chrome.tabs.sendMessage(tabId, { action: GATHER_FORMS_ACTION, tabUrl: tab.url }, function(response) {
+					// check form actions for vulnerabilities
+					chrome.tabs.sendMessage(tabId, { action: GATHER_FORMS_ACTION, tabUrl: tab.url }, function(response) {
 
-					if (!!response && !!response.formActions) {
-						var formActions = response.formActions
-						if (formActions.length > 0) {
-								for (var i = 0; i < formActions.length; i++) {
-									evaluateVulnerabilities(checkCredentials(items), tab, formActions[i])
-								}
+						if (!!response && !!response.formActions) {
+							var formActions = response.formActions
+							if (formActions.length > 0) {
+									for (var i = 0; i < formActions.length; i++) {
+										evaluateVulnerabilities(credentialed, tab, formActions[i])
+									}
+							}
 						}
-					}
-				});
+					});
 
-				// uses the tab url to evaluate
-				// check if any values are undefined
-				// console.log("items", items);
-				evaluateVulnerabilities(checkCredentials(items), tab, tab.url)
+					// check tab url for vulnerabilities
+					evaluateVulnerabilities(credentialed, tab, tab.url)
+				} else {
+					getCredentials(tab)
+				}
 			});
 		}
 	})
 });
 
-function checkCredentials(items) {
-	"use strict";
-
-	var noUsername = items.contrast_username === undefined || items.contrast_username === '',
-		noServiceKey = items.contrast_service_key === undefined || items.contrast_service_key === '',
-		noApiKey = items.contrast_api_key === undefined || items.contrast_api_key === '',
-		noTeamserverUrl = items.teamserver_url === undefined || items.teamserver_url === ''
-
-		return noUsername || noServiceKey || noApiKey || noTeamserverUrl
-}
-
-function evaluateVulnerabilities(needsCredentials, tab, requestURL) {
-	"use strict";
+/**
+ * evaluateVulnerabilities - method used by tab url, xhr and form actions to check TS for vulnerabilities
+ *
+ * @param  {Boolean} hasCredentials if the user has credentialed the extension
+ * @param  {Object} tab            Gives the state of the current tab
+ * @param  {String} requestURL     the url that will be queried to TS
+ * @return {void}
+ */
+function evaluateVulnerabilities(hasCredentials, tab, requestURL) {
 
 	if (requestURL.includes("Contrast/api/ng")) {
 		return;
 	}
-	else if (!needsCredentials) {
+	else if (hasCredentials) {
 		var url = new URL(requestURL)
-		// console.log("request url", url);
-		getOrganizationVulnerabilityesIds(url.pathname, function () {
+
+		getOrganizationVulnerabilityesIds(url.pathname, function() {
 			return function (e) {
-				var xhr = e.currentTarget, json;
-				if (xhr.readyState === 4) {
-					if (xhr.responseText !== "") {
-						json = JSON.parse(xhr.responseText);
-						if (json.traces && json.traces.length > 0) {
-							// console.log("json returned in evaluateVulnerabilities", json);
-							if (chrome.runtime.lastError) {
-								return;
-							}
-							// if (tab.index >= 0) { // tab is visible
-							// 	chrome.browserAction.setBadgeBackgroundColor({ color: CONTRAST_ICON_BADGE_BACKGROUND });
-							// 	chrome.browserAction.setBadgeText({ tabId: tab.id, text: json.traces.length.toString() });
-							// }
-							setToStorage(json.traces, tab, true)
+				var xhr = e.currentTarget;
+				if (xhr.readyState === 4 && xhr.responseText !== "") {
+
+					var json = JSON.parse(xhr.responseText);
+					if (json.traces && json.traces.length > 0) {
+						if (chrome.runtime.lastError) {
+							return;
 						}
+						setToStorage(json.traces, tab)
 					}
+
 				}
 			};
 		});
@@ -114,50 +123,75 @@ function evaluateVulnerabilities(needsCredentials, tab, requestURL) {
 	}
 }
 
-function getCredentials(tab) {
-	var url = new URL(tab.url);
-	if (VALID_TEAMSERVER_HOSTNAMES.includes(url.hostname)
-		&& (tab.url.endsWith(TEAMSERVER_ACCOUNT_PATH_SUFFIX) || tab.url.endsWith(TEAMSERVER_PROFILE_PATH_SUFFIX))
-		&& tab.url.indexOf(TEAMSERVER_INDEX_PATH_SUFFIX) !== -1) {
-
-		chrome.browserAction.setBadgeBackgroundColor({ color: CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_BACKGROUND });
-		chrome.browserAction.setBadgeText({ tabId: tab.id, text: CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_TEXT });
-
-	} else {
-		chrome.browserAction.getBadgeText({ tabId: tab.id }, function (result) {
-			if (result === CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_TEXT) {
-				chrome.browserAction.setBadgeText({ tabId: tab.id, text: '' });
-			}
+/**
+ * updateTabBadge - updates the extension badge on the toolbar
+ *
+ * @param  {Object} tab    Gives the state of the current tab
+ * @param  {Integer} count the number of vulnerabilities found
+ * @return {void}
+ */
+function updateTabBadge(tab, count) {
+	if (tab.index >= 0) { // tab is visible
+		chrome.browserAction.setBadgeBackgroundColor({
+			color: CONTRAST_ICON_BADGE_BACKGROUND
+		});
+		chrome.browserAction.setBadgeText({
+			tabId: tab.id,
+			text: count.toString(),
 		});
 	}
 }
 
-function updateTabBadge(tab, number) {
-	chrome.browserAction.setBadgeBackgroundColor({
-		color: CONTRAST_ICON_BADGE_BACKGROUND
-	});
-	chrome.browserAction.setBadgeText({
-		tabId: tab.id,
-		text: number.toString(),
-	});
+/**
+ * setToStorage - syncs the trace ids of found vulnerabilities to storage
+ *
+ * @param  {Array} foundTraces - trace ids of vulnerabilities found
+ * @param  {Object} tab             Gives the state of the current tab
+ * @return {void}
+ */
+function setToStorage(foundTraces, tab) {
+	buildVulnerabilitiesArray(foundTraces)
+	.then(function(vulnerabilities) {
+		updateTabBadge(tab, vulnerabilities.length)
+
+		let traces = {}
+		traces[STORED_TRACES_KEY] = JSON.stringify(vulnerabilities)
+
+		chrome.storage.sync.set(traces, function(result) {
+			if (chrome.runtime.lastError) {
+				console.log("error storing vulnerabilities");
+			} else {
+				console.log("vulnerabilities stored");
+			}
+		})
+	})
+	.catch(function(error) {
+		console.log("caught promise in setToStorage");
+	})
 }
 
-function setToStorage(vulnerabilities, tab, isTraces) {
-	var key = isTraces ? "traces" : "vulnerabilities"
-	// first check if there are already vulnerabilities in storage
-	var promise = new Promise(function(resolve, reject) {
-		chrome.storage.sync.get(key, function(result) {
+/**
+ * buildVulnerabilitiesArray - builds an array of trace ids, retrieving previously stored ids and deduping
+ *
+ * @param  {Array} foundTraces - trace ids of vulnerabilities found
+ * @return {Promise} - a promise that resolves to an array of deduplicated trace ids
+ */
+function buildVulnerabilitiesArray(foundTraces) {
+	return new Promise(function(resolve, reject) {
+
+		// first check if there are already vulnerabilities in storage
+		chrome.storage.sync.get(STORED_TRACES_KEY, function(result) {
 			var results;
 
-			// results have not been set yet so just pass on vulnerabilities
-			if (!result[key] || (!!result[key] && JSON.parse(result[key]).length === 0)) {
-				resolve(vulnerabilities)
+			// results have not been set yet so just pass on foundTraces
+			if (!result[STORED_TRACES_KEY] || (!!result[STORED_TRACES_KEY] && JSON.parse(result[STORED_TRACES_KEY]).length === 0)) {
+				resolve(removeDuplicatesFromArray(foundTraces))
 			} else {
 				try {
-					// add existing vulnerabilities to passed in array
-					results = JSON.parse(result[key])
-					results = results.concat(vulnerabilities)
-					resolve(results)
+					// add existing foundTraces to passed in array
+					results = JSON.parse(result[STORED_TRACES_KEY])
+					results = results.concat(foundTraces)
+					resolve(removeDuplicatesFromArray(results))
 				} catch (e) {
 					// if this errors then remove all the vulnerabilities from storage and start over
 					removeVulnerabilitiesFromStorage().then(function() {
@@ -167,76 +201,79 @@ function setToStorage(vulnerabilities, tab, isTraces) {
 			}
 		})
 	})
-
-	promise.then(function(vs) {
-		var obj = {}
-
-		// filter array for duplicates
-		vs = vs.filter(function(item, position, self) {
-			return self.indexOf(item) === position
-		})
-		updateTabBadge(tab, vs.length)
-		obj[key] = JSON.stringify(vs)
-
-		// console.log("vs in promise and obj", vs, obj);
-		chrome.storage.sync.set(obj, function(result) {
-			if (chrome.runtime.lastError) {
-				// console.log("error setting vulnerabilities", chrome.runtime.lastError);
-			} else {
-				// console.log("vulnerabilities set", vs);
-			}
-		})
-	})
-	.catch(function(error) {
-		// console.log("caught promise in setToStorage", error);
-	})
 }
 
+/**
+ * removeVulnerabilitiesFromStorage - removes all trace ids from storage
+ *
+ * @return {Promise} - returns a promise for synchronous execution
+ */
 function removeVulnerabilitiesFromStorage() {
-	// console.log("removing vulnerabilities");
 	return new Promise(function(resolve, reject) {
-		chrome.storage.sync.remove(["vulnerabilities", "traces"], function() {
+		chrome.storage.sync.remove(STORED_TRACES_KEY, function() {
 			try {
 				chrome.browserAction.setBadgeBackgroundColor({
 					color: '#00FFFFFF' // transparent
 				});
 				chrome.browserAction.setBadgeText({ text: '' });
+				console.log("vulnerabilities removed");
 			} catch (e) {
-				console.log(chrome.runtime.lastError);
+				console.log("error removing vulnerabilities");
 			}
-			// console.log("vulnerabilities removed");
 			resolve()
 		})
 	})
 }
 
+/**
+ * chrome - description
+ *
+ * @param  {Object} request a request object
+ * @param  {Object} sender  which script sent the request
+ * @param  {Function} sendResponse return information to sender, must be JSON serializable
+ * @return {Boolean} - From the documentation:
+ * https://developer.chrome.com/extensions/runtime#event-onMessage
+ * This function becomes invalid when the event listener returns, unless you return true from the event listener to indicate you wish to send a response asynchronously (this will keep the message channel open to the other end until sendResponse is called).
+ */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	// console.log("request", request);
-	if (request === "getXHRVulnerabilities") {
-		chrome.storage.sync.get("vulnerabilities", function(result) {
-			// console.log(result);
-			if (!!result && !!result.vulnerabilities) {
-				// console.log("sending response");
-				sendResponse({ vulnerabilities: JSON.parse(result.vulnerabilities) })
-			}
-		})
-	} else if (request === "getStoredTraces") {
-		chrome.storage.sync.get("traces", function(result) {
-			// console.log(result);
+	if (request === TRACES_REQUEST) {
+		chrome.storage.sync.get(STORED_TRACES_KEY, function(result) {
 			if (!!result && !!result.traces) {
-				// console.log("sending response");
 				sendResponse({ traces: JSON.parse(result.traces) })
 			}
 		})
 	}
 
-	// https://developer.chrome.com/extensions/runtime#event-onMessage
-	// This function becomes invalid when the event listener returns, unless you return true from the event listener to indicate you wish to send a response asynchronously (this will keep the message channel open to the other end until sendResponse is called).
 	return true
 })
 
+/**
+ * getCredentials - retrieves and stores credentials for user extension
+ *
+ * @param  {Object} tab Gives the state of the current tab
+ * @return {void}
+ */
+function getCredentials(tab) {
+	var url = new URL(tab.url);
+	if (VALID_TEAMSERVER_HOSTNAMES.includes(url.hostname)
+		&& (tab.url.endsWith(TEAMSERVER_ACCOUNT_PATH_SUFFIX) || tab.url.endsWith(TEAMSERVER_PROFILE_PATH_SUFFIX))
+		&& tab.url.indexOf(TEAMSERVER_INDEX_PATH_SUFFIX) !== -1) {
 
+		chrome.browserAction.setBadgeBackgroundColor({
+			color: CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_BACKGROUND
+		});
+		chrome.browserAction.setBadgeText({
+			tabId: tab.id,
+			text: CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_TEXT
+		});
 
-
-
-// https://app.contrastsecurity.com/Contrast/api/ng/04bfd6c5-b24e-4610-b8b9-bcbde09f8e15/orgtraces/ids?urls=%2FWebGoat%2Fsqlinjection
+	} else {
+		chrome.browserAction.getBadgeText({
+			tabId: tab.id
+		}, function (result) {
+			if (result === CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_TEXT) {
+				chrome.browserAction.setBadgeText({ tabId: tab.id, text: '' });
+			}
+		});
+	}
+}
--- js/util.js
+++ js/util.js
@@ -1,42 +1,44 @@
 /*global
 XMLHttpRequest, btoa, chrome
 */
-var CONTRAST_USERNAME = "contrast_username",
-  CONTRAST_SERVICE_KEY = "contrast_service_key",
-  CONTRAST_API_KEY = "contrast_api_key",
-  CONTRAST_ORG_UUID = "contrast_org_uuid",
-  TEAMSERVER_URL = "teamserver_url",
+const CONTRAST_USERNAME = "contrast_username",
+      CONTRAST_SERVICE_KEY = "contrast_service_key",
+      CONTRAST_API_KEY = "contrast_api_key",
+      CONTRAST_ORG_UUID = "contrast_org_uuid",
+      TEAMSERVER_URL = "teamserver_url",
 
-  SEVERITY_NOTE = "Note",
-  SEVERITY_LOW = "Low",
-  SEVERITY_MEDIUM = "Medium",
-  SEVERITY_HIGH = "High",
-  SEVERITY_CRITICAL = "Critical",
+      SEVERITY_NOTE = "Note",
+      SEVERITY_LOW = "Low",
+      SEVERITY_MEDIUM = "Medium",
+      SEVERITY_HIGH = "High",
+      SEVERITY_CRITICAL = "Critical",
 
-  SEVERITY_NOTE_ICON_PATH = "../img/note.png",
-  SEVERITY_LOW_ICON_PATH = "../img/low.png",
-  SEVERITY_MEDIUM_ICON_PATH = "../img/medium.png",
-  SEVERITY_HIGH_ICON_PATH = "../img/high.png",
-  SEVERITY_CRITICAL_ICON_PATH = "../img/critical.png",
+      SEVERITY_NOTE_ICON_PATH = "../img/note.png",
+      SEVERITY_LOW_ICON_PATH = "../img/low.png",
+      SEVERITY_MEDIUM_ICON_PATH = "../img/medium.png",
+      SEVERITY_HIGH_ICON_PATH = "../img/high.png",
+      SEVERITY_CRITICAL_ICON_PATH = "../img/critical.png",
 
-  HTML_BODY = "body",
-  TEAMSERVER_INDEX_PATH_SUFFIX = "/Contrast/static/ng/index.html#/",
-  TEAMSERVER_ACCOUNT_PATH_SUFFIX = "/account",
-  TEAMSERVER_PROFILE_PATH_SUFFIX = "/account/profile",
-  TEAMSERVER_API_PATH_SUFFIX = "/Contrast/api",
-  VALID_TEAMSERVER_HOSTNAMES = [
-    'localhost',
-    // 'app.contrastsecurity.com',
-    // 'apptwo.contrastsecurity.com',
-    // 'eval.contratsecurity.com'
-  ],
+      HTML_BODY = "body",
+      TEAMSERVER_INDEX_PATH_SUFFIX = "/Contrast/static/ng/index.html#/",
+      TEAMSERVER_ACCOUNT_PATH_SUFFIX = "/account",
+      TEAMSERVER_PROFILE_PATH_SUFFIX = "/account/profile",
+      TEAMSERVER_API_PATH_SUFFIX = "/Contrast/api",
+      VALID_TEAMSERVER_HOSTNAMES = [
+        'localhost',
+        // 'app.contrastsecurity.com',
+        // 'apptwo.contrastsecurity.com',
+        // 'eval.contratsecurity.com'
+      ],
 
-  CONTRAST_ICON_BADGE_BACKGROUND = "red",
-  CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_BACKGROUND = "#FFD300",
-  CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_TEXT = "*",
+      CONTRAST_ICON_BADGE_BACKGROUND = "red",
+      CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_BACKGROUND = "#FFD300",
+      CONTRAST_ICON_BADGE_CONFIGURE_EXTENSION_TEXT = "*",
 
-  LISTENING_ON_DOMAIN = "http://localhost/*",
-  GATHER_FORMS_ACTION = "gatherForms";
+      LISTENING_ON_DOMAIN = "http://localhost/*",
+      GATHER_FORMS_ACTION = "gatherForms",
+      STORED_TRACES_KEY   = "traces",
+      TRACES_REQUEST      = "getStoredTraces";
 
 // --------- HELPER FUNCTIONS -------------
 function sendXhr(url, params, authHeader, apiKey, onReadyStateChangeCallback) {
@@ -89,23 +91,6 @@ function getVulnerabilityTeamserverUrl(teamserverUrl, orgUuid, traceUuid) {
 
 // --------- HELPER FUNCTIONS -------------
 
-function getAllOrganizationVulnerabilties(url, onReadyStateChangeCallback) {
-  "use strict";
-  chrome.storage.sync.get([CONTRAST_USERNAME,
-    CONTRAST_SERVICE_KEY,
-    CONTRAST_API_KEY,
-    CONTRAST_ORG_UUID,
-    TEAMSERVER_URL], function (items) {
-      // var url = getOrganizationVulnerabiliesByFilterUrl(items[TEAMSERVER_URL], items[CONTRAST_ORG_UUID]),
-      var url = getOrganizationVulnerabilitiesIdsUrl(items[TEAMSERVER_URL], items[CONTRAST_ORG_UUID]),
-        authHeader = getAuthorizationHeader(items[CONTRAST_USERNAME], items[CONTRAST_SERVICE_KEY]),
-        params = "?urls=" + btoa(url);
-      // console.log("url", url);
-      // console.log("url params", params);
-      sendXhr(url, params, authHeader, items[CONTRAST_API_KEY], onReadyStateChangeCallback);
-    });
-}
-
 function getOrganizationVulnerabilityesIds(urls, onReadyStateChangeCallback) {
   "use strict";
   chrome.storage.sync.get([CONTRAST_USERNAME,
@@ -132,4 +117,22 @@ function getVulnerabilityShort(traceUuid, onReadyStateChangeCallback) {
         authHeader = getAuthorizationHeader(items[CONTRAST_USERNAME], items[CONTRAST_SERVICE_KEY]);
       sendXhr(url, "", authHeader, items[CONTRAST_API_KEY], onReadyStateChangeCallback);
     });
+}
+
+function isCredentialed(credentials) {
+  // ES5
+  // check if any values are undefined
+  // var noUsername = items.contrast_username === undefined || items.contrast_username === '',
+  // noServiceKey = items.contrast_service_key === undefined || items.contrast_service_key === '',
+  // noApiKey = items.contrast_api_key === undefined || items.contrast_api_key === '',
+  // noTeamserverUrl = items.teamserver_url === undefined || items.teamserver_url === '',
+  // return noUsername || noServiceKey || noApiKey || noTeamserverUrl;
+
+  // ES6
+  const values = Object.values(credentials)
+  return values.length > 0 && values.every(item => !!item)
+}
+
+function removeDuplicatesFromArray(array) {
+  return array.filter((item, position, self) => self.indexOf(item) === position)
 }
