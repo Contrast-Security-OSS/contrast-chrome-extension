9d6eb994887174d9d9a6de3149633d91731a7a1d
Dave Corderman <david.corderman@contrastsecurity.com>
2018-05-11 16:12:11 -0400

    try using xvfb

--- install-selenium.sh
+++ install-selenium.sh
@@ -19,15 +19,15 @@ rm ~/chromedriver_linux64.zip
 sudo rm /usr/local/bin/chromedriver
 sudo rm /usr/local/bin/selenium-server-standalone.jar
 
-# Install dependencies.
-# sudo apt-get update
-# sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
-#
-# # Install Chrome.
-# sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-# sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-# sudo apt-get -y update
-# sudo apt-get -y install google-chrome-stable
+Install dependencies.
+sudo apt-get update
+sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
+
+# Install Chrome.
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
 
 # Install ChromeDriver.
 wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
@@ -37,8 +37,8 @@ sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
 
-# Install Selenium.
-# wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
-# sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
-# sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
-# sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
+Install Selenium.
+wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
+sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
+sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
+sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
