81e60c71c9a649628a893f8e49d2e46d2055883e
Dave Corderman <david.corderman@contrastsecurity.com>
2018-05-15 09:44:32 -0400

    added $ to travis variable calls and changed sudo command to use tee instead of sudo echo

--- install-selenium.sh
+++ install-selenium.sh
@@ -25,7 +25,7 @@ sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4 wg
 
 # Install Chrome.
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
-sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee --append /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
 sudo apt-get -y install google-chrome-stable
 
@@ -41,4 +41,4 @@ sudo chmod 0755 /usr/local/bin/chromedriver
 wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
 sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
 sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
-sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar  
+sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
