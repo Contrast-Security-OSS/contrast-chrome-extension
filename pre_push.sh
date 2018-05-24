3b60da4643a0b963941634c835215314a54ab8a0
Dave Corderman <david.corderman@contrastsecurity.com>
2018-05-24 15:38:23 -0400

    don't use file in each for loop

--- pre_push.sh
+++ pre_push.sh
@@ -7,17 +7,17 @@
 console_log_files=$(grep -lr 'console.log' js --exclude-dir jquery --exclude-dir .git)
 
 # search lines with console.log in console_log_files and remove the line
-for file in $console_log_files; do
-  gsed -i '/console.log/d' $file
+for cfile in $console_log_files; do
+  gsed -i '/console.log/d' $cfile
 done
 
 debugger_files=$(grep -lr 'debugger' js --exclude-dir jquery --exclude-dir .git)
-for file in $debugger_files; do
-  gsed -i '/debugger/d' $file
+for dfile in $debugger_files; do
+  gsed -i '/debugger/d' $dfile
 done
 
 # search for localhost in utils file
 localhost_file=$(grep -lr 'localhost' js/util.js)
-for file in $localhost_file; do
-  gsed -i '/localhost/d' $file
+for lfile in $localhost_file; do
+  gsed -i '/localhost/d' $lfile
 done
