5ed2b08440645836c82135f429b9ec2705ff5698
Dave Corderman <david.corderman@contrastsecurity.com>
2018-05-24 15:26:35 -0400

    added git hook and script for removing console logs, localhost and debuggers from js code

---
+++ scripts/pre_push.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# get files that have console.log
+console_log_files=$(grep -lr 'console.log' . --exclude-dir jquery --exclude-dir .git)
+
+# search lines with console.log in console_log_files and remove the line
+for file in $console_log_files; do
+  sed -i '/console.log/d' $file
+done
+
+debugger_files=$(grep -lr 'debugger' . --exclude-dir jquery --exclude-dir .git)
+for file in $debugger_files; do
+  sed -i '/debugger/d' $file
+done
+
+debugger_files=$(grep -lr 'debugger' . --exclude-dir jquery --exclude-dir .git)
+for file in $debugger_files; do
+  sed -i '/debugger/d' $file
+done
+
+# search for localhost in utils file
+localhost_file=$(grep -lr 'localhost' js/util.js)
+for file in $localhost_file; do
+  sed -i '/localhost/d' $file
+done
