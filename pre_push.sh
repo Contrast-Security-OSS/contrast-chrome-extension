3e213fb0ddda68076f1b73db4a4b3061065ebba1
Dave Corderman <david.corderman@contrastsecurity.com>
2018-05-24 15:36:35 -0400

    use gnu-sed for macos in pre-push script

--- pre_push.sh
+++ pre_push.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
+# NOTE: YOU MUST INSTALL GNU SED FIRST
+# With Homebrew: brew install gnu-sed
+
 # get files that have console.log
 console_log_files=$(grep -lr 'console.log' js --exclude-dir jquery --exclude-dir .git)
 
 # search lines with console.log in console_log_files and remove the line
 for file in $console_log_files; do
-  sed -i '/console.log/d' $file
+  gsed -i '/console.log/d' $file
 done
 
 debugger_files=$(grep -lr 'debugger' js --exclude-dir jquery --exclude-dir .git)
 for file in $debugger_files; do
-  sed -i '/debugger/d' $file
-done
-
-debugger_files=$(grep -lr 'debugger' js --exclude-dir jquery --exclude-dir .git)
-for file in $debugger_files; do
-  sed -i '/debugger/d' $file
+  gsed -i '/debugger/d' $file
 done
 
 # search for localhost in utils file
 localhost_file=$(grep -lr 'localhost' js/util.js)
 for file in $localhost_file; do
-  sed -i '/localhost/d' $file
+  gsed -i '/localhost/d' $file
 done
